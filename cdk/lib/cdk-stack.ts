3a7a38a48a2601d26db4d01ab7b6df7e3db7e81c
maximillianus <pradana.aditya@gmail.com>
2022-02-05 20:48:59 +0700

    add lambda function creation CDK

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,8 +1,10 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
 
 export class CdkStack extends Stack {
@@ -10,14 +12,37 @@ export class CdkStack extends Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const lambdaFunctionName = this.node.tryGetContext('lambdafn') || 'textract-upload-document';
-    const importedLambda = lambda.Function.fromFunctionArn(
-      this,
-      lambdaFunctionName,
-      `arn:aws:lambda:${Stack.of(this).region}:${Stack.of(this).account}:function:${lambdaFunctionName}`
+    //Role
+    const lambdaExecutionRole = new iam.Role(
+      this, 
+      'lambda-textract-execution-role',
+      {
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+      }
     )
+    lambdaExecutionRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
+    lambdaExecutionRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonTextractFullAccess"));
+
+    // Lambda
+    // const lambdaFunctionName = this.node.tryGetContext('lambdafn') || 'textract-upload-document';
+    // const lambdaFunction = lambda.Function.fromFunctionArn(
+    //   this,
+    //   lambdaFunctionName,
+    //   `arn:aws:lambda:${Stack.of(this).region}:${Stack.of(this).account}:function:${lambdaFunctionName}`
+    // )
     // console.log('functionName', importedLambda.functionName)
 
+    const lambdaFunction = new lambda.Function(this, 'textract-process-document', {
+      runtime: lambda.Runtime.PYTHON_3_8,
+      memorySize: 128,
+      timeout: Duration.seconds(5),
+      handler: 'lambda_process_document.lambda_handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../src/')),
+      role: lambdaExecutionRole
+    });
+
+
+    // API Gateway
     const api = new apigateway.RestApi(
       this,
       'smart-doc-api-cdk', {
@@ -31,7 +56,7 @@ export class CdkStack extends Stack {
     const identify = api.root.addResource('identify');
 
     const getLambdaIntegration = new apigateway.LambdaIntegration(
-      importedLambda,
+      lambdaFunction,
       {
         contentHandling: apigateway.ContentHandling.CONVERT_TO_TEXT,
         proxy: false,
